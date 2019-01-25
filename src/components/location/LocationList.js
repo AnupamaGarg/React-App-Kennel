145dfbc70fa66f0a3ca99ff48fc3781defe66a54
anupamagarg <dolly.singla@gmail.com>
2019-01-25 11:52:08 -0600

    added the location component responsible for list of locations

---
+++ src/components/location/LocationList.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+
+export default class Kennel extends Component {
+    render() {
+        return (
+            <div>
+                <h3>Student Kennels Locations</h3>
+                <section>
+                    <h4>Nashville North Location </h4>
+                        <p>701 Learning Way</p>
+                </section>
+                    <section>
+                        <h4>Nashville South Location </h4>
+                            <h5>500 Puppy Way</h5>
+                    </section>
+                
+        
+            </div>
+                    );
+                }
+}
